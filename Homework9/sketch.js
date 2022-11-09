f80bd3291ba386cf144e0292c2f3e39c3c880502
ana-ass <112594031+ana-ass@users.noreply.github.com>
2022-11-08 23:53:53 -0700

    Add files via upload

---
+++ Homework9/sketch.js
@@ -0,0 +1,69 @@
+var x = 100;
+var y = 100;
+
+function setup()
+{
+    createCanvas (500, 600);
+
+}
+function draw()
+{
+    background(120);
+    fill(255, 204, 153)
+    ellipse(220,150, 185, 195)
+    fill(0, 0, 0)
+    rect(120, 243, 200, 300)
+    fill(255, 204, 153)
+    noStroke();
+    triangle(166, 230, 270, 230, 223, 260 )
+    fill(0, 0, 0)
+    point(176, 145);
+    point(269, 145);
+    stroke('black');
+    strokeWeight(12)
+    line(163, 145, 193, 145);
+    line(243, 145, 273, 145);
+    strokeWeight(2);
+    strokeWeight(20);
+    point(179, 155)
+    point(254, 155)
+    strokeWeight(2)
+    line(163, 164, 193, 164)
+    line(243, 164, 273, 164)
+    fill(255, 255, 255)
+    triangle(178, 198, 258, 198, 220, 220)
+    strokeWeight(45)
+    line(297, 257, 387, 430)
+    line(388, 433, 432, 242)
+    line(135, 257, 47, 602 )
+    strokeWeight(2)
+    fill(255, 204, 153)
+    rect(382, 179, 85, 95)
+    line(403, 181, 403, 217)
+    line(427, 181, 427, 217)
+    stroke(255, 204, 153)
+    strokeWeight(20)
+    line(441, 190, 421, 111)
+    line(457, 190, 478, 111 )
+    strokeWeight(2)
+    stroke(0, 0, 0)
+    line(464, 204, 416, 224)
+    fill('blue')
+    rect(119, 540, 200, 300)
+    fill('brown')
+    triangle(158, 80, 191, 85, 127, 148 )
+    triangle(141, 80, 176, 73, 124, 150 )
+    fill(255, 255, 153)
+    triangle(141, 80, 176, 73, 196, 27 )
+    triangle(158, 73, 219, 43, 216, 121 )
+    triangle(192, 64, 255, 29, 280, 146)
+    triangle(172, 58, 295, 48, 315, 162 )
+    noFill();
+    rect(153, 130, 50, 50)
+    rect(232, 130, 50, 50)
+    line(203,155, 231, 155)
+    fill('white')
+    strokeWeight(1)
+    textSize(20);
+    text("Anabel Assante", 12, 580)
+}
